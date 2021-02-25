4c72779b75886792d9deebed83bd9740c65f263b
mylinhlac <mclac@ucsd.edu>
2021-02-25 10:28:14 -0800

    lab 8 complete

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -10,13 +10,33 @@ $(document).ready(function() {
  */
 function initializePage() {
 	console.log("Page ready");
- 	// initCamera();
- 	// initMap();
- 	initGestures();
- 	initRSVPForm();
-}
+ 	initCamera();
+ 	initMap();
+ 	initGestures()
+    $(function(){
+      $(".judge-img").bind("taphold", tapholdHandler);
 
-// init jQuery gestures  
+      function tapholdHandler(event){
+        var targetIDPrefix = event.target.id;
+        console.log("got prefix:" + targetIDPrefix);
+        $("#" + targetIDPrefix + "-bio").show();
+
+      }
+    });
+  }
+
+//   $(function(){
+//   $( "div.box" ).bind( "taphold", tapholdHandler );
+//
+//   function tapholdHandler( event ){
+//     $( event.target ).addClass( "taphold" );
+//   }
+// });
+
+ 	initRSVPForm();
+
+
+// init jQuery gestures
 function initGestures() {
 	// add gestures listener here
 }
@@ -24,4 +44,17 @@ function initGestures() {
 // init RSVP form submit listener
 function initRSVPForm() {
   // add your code here
+  $('#rsvpForm').submit(function(e) {
+
+    e.preventDefault();
+    console.log("submitting form...")
+    var rsvpEmail = $('#rsvpEmail').val();
+
+    $.post('addRSVP', {rsvpEmail: rsvpEmail}, postCallback);
+  });
+
+function postCallback(res){
+  alert("RSVP form successfully submitted!");
+  $('#rsvpEmail').val('');
 }
+  }
--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,14 @@
 function initMap() {
 	// add your code here
+  L.mapquest.key = 'qrC96sz4t52Y9DrJpEQ2beuDk1cl9cW8';
+
+  // 'map' refers to a <div> elemenet with the ID map
+  var map = L.mapquest.map('map', {
+    center: [32.88, -117.236],
+    layers: L.mapquest.tileLayer('map'),
+    zoom: 12,
+    zoomControl: false
+  });
+
+  L.marker([32.88, -117.236]).addTo(map);
 }
